ae7962e122cf39b4eeff0bafe6fce0b4ec6cf5c7
Alex <ipritoflex@yandex.ru>
2016-10-15 00:59:01 +0300

    Bug fixes

--- src/foreground/GameMap.js
+++ src/foreground/GameMap.js
@@ -101,12 +101,13 @@ class GameMap extends IslandsMap {
     this.score++;
     this.scoreView.setScore(this.score);
     
-    let scoreIncrementerView = new ScoreIncrementer();
     if (isWebGLRenderer) {
+      let scoreIncrementerView = new ScoreIncrementer();
       scoreIncrementerView.addScore(1, nearestAnimal.animalType);
       this.addChild(scoreIncrementerView);
     }
     console.log('Score:', this.score);
+    
     this.animationAttractor.append(nearestAnimal.getRebornNumber(), nearestAnimal, function animate(animal) {
       return animal.explode();
     });
