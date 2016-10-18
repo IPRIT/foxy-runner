88619b18b7359c8056b6be48af0eef1caef38997
Alex <ipritoflex@yandex.ru>
2016-10-19 00:57:41 +0300

    Added resources

--- src/UI/boards/app.js
+++ src/UI/boards/app.js
@@ -1,11 +1,9 @@
 import angular from 'angular';
-import * as ngTouch from 'ngtouch';
 import * as controller from './controller';
 import * as directive from './directive';
 
 let app = angular.module('FoxyRunnerApp', [
-  'ng',
-  'ngTouch'
+  'ng'
 ]);
 
 app.controller('PageCtrl', controller.PageCtrl);
