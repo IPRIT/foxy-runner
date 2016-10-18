029a456bb494f035e7d26705d2c856680fdb0fa5
Alex <ipritoflex@yandex.ru>
2016-10-19 01:03:16 +0300

    Added resources

--- src/UI/boards/controller/PageCtrl.js
+++ src/UI/boards/controller/PageCtrl.js
@@ -5,24 +5,13 @@ export default class PageCtrl {
   static $inject = [ '$scope', '$rootScope', '$timeout' ];
   
   constructor($scope, $rootScope, $timeout) {
+    deap.extend(this, {
+      $timeout
+    });
     this.pageType = 'greeting';
-    this.logoSize = 50;
-    
-    $timeout(() => {
-      this.logoSize = 30;
-    }, 1500);
-  
-    /*deap.extend($scope, {
-      vm: this
-    });*/
   }
   
-  test() {
-    console.log('Works');
-    return this.test2();
-  }
-  
-  test2() {
-    return 'Another fn works'
+  play() {
+    window.game.start();
   }
 }
