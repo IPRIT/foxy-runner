e1cf8fd116f995ea861b0f4cf702293ec45704f9
Alex <ipritoflex@yandex.ru>
2016-10-20 23:36:36 +0300

    Bug fixes

--- src/UI/boards/app.js
+++ src/UI/boards/app.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
+import * as ngTouch from 'ngtouch';
 import * as controller from './controller';
 import * as directive from './directive';
 
 let app = angular.module('FoxyRunnerApp', [
-  'ng'
+  'ng',
+  'ngTouch'
 ]);
 
 app.controller('PageCtrl', controller.PageCtrl);
