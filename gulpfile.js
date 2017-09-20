003695c0c245f85b4b95c062b040ab8a0966e67b
Alex <ipritoflex@yandex.ru>
2017-09-20 22:57:21 +0300

    added runtime script

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,14 @@ require('babel-core/register');
 
 var gulp        = require('gulp')
   , babel       = require('gulp-babel')
-  , uglify 	= require('gulp-uglify')
+  , uglify 	    = require('gulp-uglify')
   , del         = require('del')
-  , watch 	= require('gulp-watch')
+  , watch 	    = require('gulp-watch')
   , batch       = require('gulp-batch')
   , browserify	= require('browserify')
-  , concat 	= require('gulp-concat')
-  , source 	= require('vinyl-source-stream')
-  , buffer 	= require('vinyl-buffer')
+  , concat 	    = require('gulp-concat')
+  , source 	    = require('vinyl-source-stream')
+  , buffer 	    = require('vinyl-buffer')
   , mergeStream = require('merge-stream');
 
 gulp.task('build:es6-commonjs', function() {
@@ -23,26 +23,27 @@ gulp.task('build:es6-commonjs', function() {
 });
 
 gulp.task('build:js', [ 'build:es6-commonjs' ], function() {
-    return mergeStream(
-        gulp.src([
-	  './node_modules/pixi.js/bin/pixi.min.js'
-	]),
-    	browserify('./dist/core/temp/game.js')
-      	    .bundle()
-      	    .pipe(source('tricky-foxy.min.js'))
-      	    .pipe(buffer())
-      	    .pipe(uglify())
-    )
+  return mergeStream(
+    gulp.src([
+      './node_modules/pixi.js/bin/pixi.min.js',
+      './src/lib/runtime.js'
+    ]),
+    browserify('./dist/core/temp/game.js')
+      .bundle()
+      .pipe(source('tricky-foxy.min.js'))
+      .pipe(buffer())
+      .pipe(uglify())
+  )
     .pipe(concat('tricky-foxy.min.js'))
     .pipe(gulp.dest('./dist/core'));
 });
 
 gulp.task('clean', function() {
-    return del([ 'dist/core' ]);
+  return del([ 'dist/core' ]);
 });
 
 gulp.task('build', [ 'build:js' ], function () {
-    return del([ 'dist/core/temp' ]);
+  return del([ 'dist/core/temp' ]);
 });
 
 gulp.task('watch', ['build'], function () {
