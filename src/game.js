84c2f9ae15aea32c3b0cb7960bf51d3f53877716
BuildTools <ipritoflex@yandex.ru>
2023-10-22 19:38:18 +0300

    fix: minor improvements

--- src/game.js
+++ src/game.js
@@ -29,6 +29,8 @@ function setupSettings() {
   let { gameMusic2, gameSounds } = settings || {};
   window.gameMusic = isType('Boolean', gameMusic2) ? gameMusic2 : false;
   window.gameSounds = isType('Boolean', gameSounds) ? gameSounds : true;
+
+  console.log('gameMusic, gameSounds', gameMusic, gameSounds)
   if (!window.gameMusic) {
     angular.element(document.querySelector('.button-music')).addClass('music-off');
   }
